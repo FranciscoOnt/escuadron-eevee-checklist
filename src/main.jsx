d1248c50828d551cedf5fb536d4a19774ca72915
Francisco Ontiveros <33066750+FranciscoOnt@users.noreply.github.com>
2025-05-30 17:23:23 -0600

    update readme

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import PrintableApp from './printable'
 import InteractiveApp from './interactive'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/escuadron-eevee-checklist'>
-    <Routes>
-      <Route path="/" element={<InteractiveApp />} />
-      <Route path="/hoja" element={<PrintableApp />} />
-    </Routes>
-    </BrowserRouter>
+    <HashRouter basename='/escuadron-eevee-checklist'>
+      <Routes>
+        <Route path="/" element={<InteractiveApp />} />
+        <Route path="/hoja" element={<PrintableApp />} />
+      </Routes>
+    </HashRouter >
   </StrictMode>,
 )
